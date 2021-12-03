4b53d115ba41ad319f1fe16e46617f81e86cc96a
Dennis Magnusson <d@a.local>
2021-12-03 19:46:11 +0100

    Initial commit ish

---
+++ desktop/src/tech/dennismagnusson/wintergame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tech.dennismagnusson.wintergame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tech.dennismagnusson.wintergame.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
---
+++ html/src/tech/dennismagnusson/wintergame/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package tech.dennismagnusson.wintergame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import tech.dennismagnusson.wintergame.MainGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MainGame();
+        }
+}
